79cc9d392f73407cd76c2986d91c469b95d0a451
Prakash Naidu <prakashnaidu@Prakashs-MacBook-Pro.local>
2021-12-26 20:09:57 +0530

    refactor

--- src/components/layout/sidebar/index.js
+++ src/components/layout/sidebar/index.js
@@ -9,7 +9,7 @@ import BookmarkIcon from '../../../components/icons/bookmark';
 import PencilIcon from '../../../components/icons/pencil';
 import ArchiveIcon from '../../../components/icons/archive';
 
-const mockLabels = Array.from({ length: 20 }).map((_, index) => ({
+const mockLabels = Array.from({ length: 10 }).map((_, index) => ({
   label: `Bookmark label ${index + 1}`,
   order: index,
 }));
